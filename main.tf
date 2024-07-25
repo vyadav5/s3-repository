c99a5b971903884a10da67341d80ca721d00e6af
Vidhi Yadav <79339896+vyadav5@users.noreply.github.com>
2024-07-26 01:33:49 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,32 @@
+provider "aws" {
+  region = "us-west-2" # Update to your region
+}
+
+resource "aws_iam_user" "user" {
+  name = "access-user-${var.user_email}"
+}
+
+resource "aws_iam_user_policy" "user_policy" {
+  name   = "S3AccessPolicy-${var.user_email}"
+  user   = aws_iam_user.user.name
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect   = "Allow"
+        Action   = "s3:*"
+        Resource = [
+          "arn:aws:s3:::${var.bucket_name}",
+          "arn:aws:s3:::${var.bucket_name}/*"
+        ]
+      }
+    ]
+  })
+}
+
+output "user_arn" {
+  value = aws_iam_user.user.arn
+}
+
+variable "bucket_name" {}
+variable "user_email" {}
